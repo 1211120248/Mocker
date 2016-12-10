df76483cc10e235c275b3268fcd0a30c8108fae9
mocker <admin>
2016-12-10 15:02:54 +0800

    简单集成Spring Boot

---
+++ mocker-web/src/main/java/com/mocker/Application.java
@@ -0,0 +1,19 @@
+package com.mocker;/**
+ * Created by sunwukong on 16/12/10.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mocker
+ * @Date 2016-12-10 下午2:40
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
